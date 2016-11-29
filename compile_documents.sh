387ffea4f706a2db60c6fb19710cc088a40d8a5c
AlbertPumarola <albert.pumarola.peris@gmail.com>
2016-11-29 09:23:19 +0100

    new structure

---
+++ compile_documents.sh
@@ -0,0 +1,16 @@
+#!/bin/bash    
+cd latex
+pdflatex main.tex
+mv main.pdf ../full_document.pdf
+extension='.pdf'
+for file in Chapters/*.tex; 
+do
+    filepath="${file%.*}"
+    filename=$(basename "$file")
+    fname="${filename%.*}"
+    pdflatex -jobname=$fname "\includeonly{$filepath}\input{main}"; 
+    pdffilename=$fname$extension
+    pdftk $pdffilename cat 1-end output "../individual_chapters/$pdffilename"
+    rm $fname*
+done
+cd ..
--- generate_individual_chapters.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash    
-for file in ./Chapters/*.tex; 
-do
-    filename=$(basename "$file")
-    fname="${filename%.*}"
-    pdflatex -jobname=$fname "\includeonly{$file}\input{main}"; 
-    mv $fname* PDF_chapters/;
-done
